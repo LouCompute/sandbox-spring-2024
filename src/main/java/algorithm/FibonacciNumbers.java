d58f519f8584e9a0a580412f103c9ddce64d0d25
LouCompute <Louiegarcia711@gmail.com>
2024-01-23 19:31:48 -0700

    Adding fib

---
+++ src/main/java/algorithm/FibonacciNumbers.java
@@ -0,0 +1,22 @@
+package algorithm;
+
+public class FibonacciNumbers {
+
+    public static void main(String[] args) {
+        FibonacciNumbers fn = new FibonacciNumbers();
+
+        for (int x = 0; x <= 15; x++) {
+            System.out.println("fib(" + x + ") = " + fn.fib(x));
+        }
+    }
+        private long fib( int x){
+            if (x == 0) {
+                return 0;
+            }
+            if (x == 1) {
+                return 1;
+            }
+
+            return fib(x - 1) + fib(x - 2);
+        }
+}
