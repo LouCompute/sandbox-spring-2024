bbfdfaa23d98e83b7434d6b203ed736070e241a5
LouCompute <Louiegarcia711@gmail.com>
2024-01-25 23:03:51 -0700

    Expanding Fib, Calculator with Unit test, and pom update

--- src/test/java/unittests/CalcuatorTest.java
+++ src/test/java/unittests/CalcuatorTest.java
@@ -14,39 +14,51 @@ class CalcuatorTest {
         int expected = 4;
         int actual = new Calcuator().add(2,2);
         assertEquals(expected, actual);
+
     }
+
     @org.junit.jupiter.api.Test
     void testAdd() {
+
         int expected = 6;
-        int actual = new Calcuator().add(3,3);
+        int actual = new Calcuator().add(2,2,2);
         assertEquals(expected, actual);
+
     }
 
     @org.junit.jupiter.api.Test
     void subtract() {
+
         int expected = 2;
-        int actual = new Calcuator().add(4,2);
+        int actual = new Calcuator().subtract(4,2);
         assertEquals(expected, actual);
+
     }
 
     @org.junit.jupiter.api.Test
     void multiply() {
+
         int expected = 4;
-        int actual = new Calcuator().add(2,2);
+        int actual = new Calcuator().multiply(2,2);
         assertEquals(expected, actual);
+
     }
 
     @org.junit.jupiter.api.Test
     void divide() {
-        int expected = 8;
-        int actual = new Calcuator().add(4,2);
+
+        int expected = 4;
+        int actual = new Calcuator().divide(8,2);
         assertEquals(expected, actual);
+
     }
 
     @org.junit.jupiter.api.Test
     void divideByZero() {
+
         assertThrows(ArithmeticException.class, () -> {
             new Calcuator().divide(0,0);
+
         });
     }
 }
